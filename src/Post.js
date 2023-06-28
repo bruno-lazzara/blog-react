4fd2d66c640ef05d89be811a1d1d26a3be1e58ad
Bruno Lazzara Queiroz <desenvolvimentocds-pge@pge.ba.gov.br>
2023-06-27 22:00:56 -0300

    Load posts from database and post page

--- src/Post.js
+++ src/Post.js
@@ -1,17 +1,24 @@
-export default function Post() {
-    return (
-        <div className="post">
-        <div className="image">
-          <img src="https://cdn.psychologytoday.com/sites/default/files/styles/image-article_inline_full_caption/public/field_blog_entry_images/2019-04/sleep-01_0.png?itok=LZ3nmqOf" alt="" />
-        </div>
-        <div className="texts">
-          <h2>What Happens to Your Brain When You Sleep?</h2>
-          <p className="info">
-            <a className="author">Bruno Lazzara</a>
-            <time>2023-06-26 21:15</time>
-          </p>
-          <p className="summary">Is sleep really the cousin of death? Or an essential part of life?</p>
-        </div>
+import { format } from "date-fns";
+import { Link } from "react-router-dom";
+
+export default function Post({ _id, title, summary, cover, createdAt, author }) {
+  return (
+    <div className="post">
+      <div className="image">
+        <Link to={'/post/' + _id}>
+          <img src={'http://localhost:4000/' + cover} alt="" />
+        </Link>
       </div>
-    );
+      <div className="texts">
+        <Link to={'/post/' + _id}>
+          <h2>{title}</h2>
+        </Link>
+        <p className="info">
+          <a className="author">{author.username}</a>
+          <time>{format(new Date(createdAt), 'dd-MM-yyyy HH:mm')}</time>
+        </p>
+        <p className="summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
+import { Navigate } from "react-router-dom";
 
 const modules = {
     toolbar: [
@@ -22,8 +23,33 @@ export default function CreatePost() {
     const [title, setTitle] = useState('');
     const [summary, setSummary] = useState('');
     const [content, setContent] = useState('');
+    const [files, setFiles] = useState('');
+    const [redirect, setRedirect] = useState(false);
+    
+    async function createNewPost(ev) {
+        const data = new FormData();
+        data.set('title', title);
+        data.set('summary', summary);
+        data.set('content', content);
+        data.set('file', files[0]);
+        ev.preventDefault();
+        const response = await fetch('http://localhost:4000/post', {
+            method: 'POST',
+            body: data,
+            credentials: 'include'
+        });
+        
+        if (response.ok) {
+            setRedirect(true);
+        }
+    }
+
+    if (redirect) {
+        return <Navigate to={'/'} />
+    }
+
     return (
-        <form>
+        <form onSubmit={createNewPost}>
             <input type="title"
                 placeholder={'Title'}
                 value={title}
@@ -34,7 +60,8 @@ export default function CreatePost() {
                 value={summary}
                 onChange={ev => setSummary(ev.target.value)} />
 
-            <input type="file" />
+            <input type="file"
+                onChange={ev => setFiles(ev.target.files)} />
 
             <ReactQuill value={content}
                 modules={modules}
